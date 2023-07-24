720989d2c4bceb34e6abaab66f98390ff3869c71
Matthew-Graves-Git <matthew.graves.cs@gmail.com>
2023-07-24 17:58:52 -0400

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <SortingVisualizer/>
 );
 
 
